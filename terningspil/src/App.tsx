8ddcb546160d6dde06faa504f8e2814309b07504
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2026-01-15 13:43:16 +0100

    Mange changes

--- terningspil/src/App.tsx
+++ terningspil/src/App.tsx
@@ -9,10 +9,6 @@ function App() {
   const [guess, setGuess] = useState('');
   const [player, setPlayer] = useState(0);
   const [systemMessage, setSystemMessage] = useState('');
-  useEffect(() => {
-    console.log(score);
-    console.log(player, 'player <---');
-  }, [score, player]);
 
   const handleRoll = () => {
     const roll = Math.floor(Math.random() * 6 + 1);
@@ -22,9 +18,9 @@ function App() {
       const copy = [...prevScore];
       copy[activePlayer] = roll;
 
-      if (copy.indexOf(0) === -1) {
-        checkWin(activePlayer);
-      }
+      // if (copy.indexOf(0) === -1) {
+      //   checkWin(activePlayer);
+      // }
 
       return copy;
     });
@@ -34,16 +30,20 @@ function App() {
 
   function handleGuess(guess) {
     setGuess((prevGuess) => (prevGuess = guess));
+    setGamePhase('rul');
   }
 
   function checkWin(player: number) {
     //check hvad overunder er på og alt efter hvad den er så sammenlign dem ved at checke hvem der er den aktive gætter(kan kigges ved at se på player variablen og så lægge 1 til) og vis en `Spiller ${player+1} vandt/tabte`
     console.log('still checking wins');
+    console.log('player =', player);
     const opponent = player === 0 ? 1 : 0;
     const playerRoll = score[player];
     const oppoRoll = score[opponent];
     if (playerRoll === oppoRoll) {
       setSystemMessage('Uafgjort!');
+      setGamePhase('Game over');
+
       return;
     }
 
@@ -51,8 +51,19 @@ function App() {
     const guessedCorrect = guess === 'over' ? playerRolledHigher : !playerRolledHigher;
     const winner = guessedCorrect ? player : opponent;
 
+    console.log(`spiller ${winner + 1} vandt`);
     setSystemMessage(`spiller ${winner + 1} vandt`);
+    setGamePhase('Game over');
   }
+
+  useEffect(() => {
+    if (score.indexOf(0) > -1) return;
+    checkWin(player);
+
+    console.log(score);
+    console.log(player, 'player <---');
+  }, [score, player]);
+
   //todo: COMPONENTS og gamechange
 
   return (
